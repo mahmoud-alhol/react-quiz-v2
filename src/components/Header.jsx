44e1f2a9e00ac147d997bb3f549378dad6c7ec10
mahmoud-alhol <mahmoud.asem.w@gmail.com>
2024-08-29 13:56:32 +0300

    flashing header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,24 @@
+import { useEffect, useState } from "react";
+
 function Header() {
+  const [isStyledH, setIsStyledH] = useState(true);
+  const [isStyledImg, setIsStyledImg] = useState(false);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIsStyledImg((prevIsStyled) => !prevIsStyled);
+      setIsStyledH((prevIsStyled) => !prevIsStyled);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const styleH = isStyledH ? { color: "#fffb00", textShadow: "0px 0px 30px #fff349" } : {};
+  const styleImg = isStyledImg ? { filter: "drop-shadow(0 0 10px #61dafb" } : {filter: "grayscale(100%)"};
   return (
-    <header className='app-header'>
-      <img src='logo512.png' alt='React logo' />
-      <h1>The React Quiz</h1>
+    <header className="app-header">
+      <img src="logo512.png" alt="React logo" style={styleImg} />
+      <h1 style={ styleH }>The React Quiz</h1>
     </header>
   );
 }
